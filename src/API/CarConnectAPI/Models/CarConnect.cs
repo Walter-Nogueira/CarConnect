b138c47b9969c42c84ec1bb9749069d91d00f850
Walter Nogueira <netoneto796@gmail.com>
2023-05-18 00:51:06 -0300

    little changes

---
+++ src/API/CarConnectAPI/Models/CarConnect.cs
@@ -0,0 +1,10 @@
+namespace CarConnectAPI.Models
+{
+    public class CarConnect
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
